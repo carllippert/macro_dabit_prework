8c459fab67821b3c0c35c99d1777496013e32ee9
Carl Lippert <carl.lippert@gmail.com>
2022-07-04 09:18:00 -0500

    fin

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -2,6 +2,19 @@
 pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract NDToken is ERC20 {
+    constructor(string memory name, string memory symbol) ERC20(name, symbol) {
+        _mint(msg.sender, 100000 * (10 ** 18));
+    }
+}
+
 
 contract Token {
   string public name = "Carl Lipper Token";
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -3,10 +3,7 @@ const hre = require("hardhat");
 async function main() {
   const [deployer] = await hre.ethers.getSigners();
 
-  console.log(
-    "Deploying contracts with the account:",
-    deployer.address
-  );
+  console.log("Deploying contracts with the account:", deployer.address);
 
   const Greeter = await hre.ethers.getContractFactory("Greeter");
   const greeter = await Greeter.deploy("Hello, World!");
@@ -14,16 +11,21 @@ async function main() {
   const Token = await hre.ethers.getContractFactory("Token");
   const token = await Token.deploy();
 
+  const NDToken = await hre.ethers.getContractFactory("NDToken");
+  const ndToken = await NDToken.deploy("Nader Dabit Token", "NDT");
+
   await greeter.deployed();
   await token.deployed();
+  await ndToken.deployed();
 
   console.log("Greeter deployed to:", greeter.address);
   console.log("Token deployed to:", token.address);
+  console.log("ndToken deployed to:", ndToken.address);
 }
 
 main()
   .then(() => process.exit(0))
-  .catch(error => {
+  .catch((error) => {
     console.error(error);
     process.exit(1);
   });
